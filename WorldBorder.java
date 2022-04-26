b49e407be01a0ad3811e1b589658bc0cd64938d0
michael bentein <michael.bentein@gmail.com>
2022-04-26 11:16:31 +0200

    worldborder

--- WorldBorder.java
+++ WorldBorder.java
@@ -8,20 +8,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class WorldBorder extends Actor
 {
+    private int cellSizeHash;
+    private int widthHash;
+    private int heightHash;
+
     /**
      * Draw the border and move to the correct position
      */
     public void act()
     {
+        int cellSize = getGridWorld().cellSize;
+        int width = getGridWorld().area_width * cellSize;
+        int height = getGridWorld().area_height * cellSize;
+        Camera camera = getGridWorld().getCamera();
+        if(cellSizeHash != cellSize || widthHash != width || heightHash != height)
+        {
+            cellSizeHash = cellSize;
+            widthHash = width;
+            heightHash = height;
+            if(width > 0 && height > 0 )
+            {
+                setImage(createImage(width, height));
+            }
+        }
+        setLocation(-camera.cameraX + width/2, -camera.cameraY + height/2);
         
     }
     
     private GridWorld getGridWorld() {
-        
+        return (GridWorld) getWorld();
     }
     
     private GreenfootImage createImage(int width, int height) {
-        
+        GreenfootImage img = new GreenfootImage(width,height);
+        img.setColor( Color.RED);
+        img.drawRect(0, 0,width-1, height-1);
+        return img;
     }
     
 }
